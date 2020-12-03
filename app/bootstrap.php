f4ec887be230950efb09ee92392d4bb75db3489d
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-12-03 23:51:09 +0100

    load sesion helper file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,13 +1,12 @@
 <?php
 // lOAD CONFIG 
 require_once 'config/config.php';
-// Load libraries
+// Load Helpers
+require_once 'helper/url_helper.php';
+require_once 'helper/seasion_helper.php';
+
 
 // Auto LOad Core Libraries 
 spl_autoload_register(function ($className) {
   require_once 'libraries/' . $className . '.php';
 });
-
-// Load Helpers
-
-require_once 'helper/url_helper.php';
