76dc5d23807f867996e24277d2e329270842764d
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-12-03 23:10:44 +0100

    connect to the database

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 // DATABASE PARAMS 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'youssef');
-define('DB_PASS', 'koko/8520*K?');
+define('DB_PASS', '123456');
 define('DB_NAME', 'blog');
 
 // App Root 
