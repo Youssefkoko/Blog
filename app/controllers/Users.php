085b3f6f999dff702bc668472600793dd7d85389
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-12-04 13:52:30 +0100

    create logout function and userSesion and isLogedIn

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -126,7 +126,7 @@ class Users extends Controller
         $loggedInUser =  $this->userModel->login($data['email'], $data['password']);
         if ($loggedInUser) {
           // Create Sesion
-          die('success..');
+          $this->createUserSesion($loggedInUser);
         } else {
           $data['password_err'] = 'Password Incorrect';
           $this->view('users/login', $data);
@@ -155,6 +155,29 @@ class Users extends Controller
 
       // Load view
       $this->view('users/login', $data);
+    }
+  }
+  public function createUserSesion($user)
+  {
+    $_SESSION['user_id'] = $user->id;
+    $_SESSION['user_email'] = $user->email;
+    $_SESSION['user_name'] = $user->name;
+    redirect('pages/index');
+  }
+  public function logout()
+  {
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_email']);
+    unset($_SESSION['user_name']);
+    session_destroy();
+    redirect('users/login');
+  }
+  public function isLoggedIn()
+  {
+    if (isset($_SESSION['user_id'])) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
