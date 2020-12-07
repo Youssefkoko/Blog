1c753d2ff14e830678c94a3049a401e8d8299f0c
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-12-07 23:14:56 +0100

    create methods update and delete methods

--- app/models/post.php
+++ app/models/post.php
@@ -39,4 +39,47 @@ class Post
       return false;
     }
   }
+  public function getPostById($id)
+  {
+    $this->db->query('SELECT * FROM posts WHERE id = :id');
+    $this->db->bind(':id', $id);
+    $row = $this->db->single();
+    return $row;
+  }
+
+  // uPDATE 
+  public function updatePost($data)
+  {
+    // Register User 
+
+    $this->db->query("UPDATE posts SET title = :title, body = :body WHERE id = :id");
+    // Bind Values 
+    $this->db->bind(":id", $data['id']);
+    $this->db->bind(":title", $data['title']);
+    $this->db->bind(":body", $data['body']);
+    // Execute 
+    if ($this->db->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Delete Function 
+
+  public function deletePost($id)
+  {
+    // Register User 
+
+    $this->db->query("DELETE FROM posts  WHERE id = :id");
+    // Bind Values 
+    $this->db->bind(":id", $id);
+
+    // Execute 
+    if ($this->db->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
